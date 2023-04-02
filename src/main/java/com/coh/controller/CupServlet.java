a31a45868a6b39dce939bf07e296bd81d1256009
taoli-ax <deuta-tao@outlook.com>
2023-04-03 01:32:54 +0800

    Again intergate Mybatis to Spring and PageNation basic

---
+++ src/main/java/com/coh/controller/CupServlet.java
@@ -0,0 +1,75 @@
+package com.coh.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.coh.pojo.Cup;
+import com.coh.pojo.PageInfo;
+import com.coh.service.CupService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/cup")
+public class CupServlet extends HttpServlet {
+    CupService cupService;
+    @Override
+    public void init() throws ServletException {
+        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("app.xml");
+        cupService=(CupService)applicationContext.getBean("cupServiceImpl");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       doGet(req,resp);
+    }
+
+    public void  list(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        List<Cup> cupList=cupService.findAll();
+        System.out.println(cupList);
+        String string =JSON.toJSONString(cupList);
+        PrintWriter printWriter=resp.getWriter();
+        printWriter.println(string);
+        printWriter.flush();
+        printWriter.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method=req.getParameter("method");
+
+        switch (method){
+            case "list":
+                list(req,resp);
+                break;
+            case "findByPage":
+                findByPage(req,resp);
+        }
+    }
+    private void findByPage(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String pageNumber=req.getParameter("pageNumber");
+        int pageNum=1;
+        if(pageNumber!=null && !"".equals(pageNumber)){
+            pageNum=Integer.parseInt(pageNumber);
+        }
+        PageInfo pageInfo=new PageInfo();
+        pageInfo.setPageNumber(pageNum);
+        List<Cup> cups=cupService.findByPage(pageInfo);
+        Map<String ,Object> map=new HashMap<>();
+        map.put("pageInfo",pageInfo);
+        map.put("cups",cups);
+
+        String string =JSON.toJSONString(map);
+        PrintWriter printWriter=resp.getWriter();
+        printWriter.println(string);
+        printWriter.flush();
+        printWriter.close();
+    }
+}
---
+++ src/main/java/com/coh/service/impl/CupServiceImpl.java
@@ -0,0 +1,50 @@
+package com.coh.service.impl;
+
+import com.coh.dao.CupDao;
+import com.coh.pojo.Cup;
+import com.coh.pojo.CupExample;
+import com.coh.pojo.PageInfo;
+import com.coh.service.CupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.List;
+@Service
+public class CupServiceImpl implements CupService {
+    @Autowired
+    private CupDao cupDao;
+    public CupServiceImpl(){
+
+    }
+
+    public CupServiceImpl(CupDao cupDao) {
+        this.cupDao = cupDao;
+    }
+
+    public void setCupDao(CupDao cupDao) {
+        this.cupDao = cupDao;
+    }
+
+    @Override
+    public List<Cup> findAll() {
+        return cupDao.selectByExample(null);
+    }
+
+    @Override
+    public List<Cup> findByPage(PageInfo pageInfo) {
+        CupExample cupExample=new CupExample();
+        // offset位移 =（当前页-1） * 每页条数
+        cupExample.setOffset((long) ((long) (pageInfo.getPageNumber() - 1) *pageInfo.getPageSize()));
+        cupExample.setLimit(pageInfo.getPageSize());
+        // 获取总行数
+        long lines=cupDao.countByExample(null);
+        pageInfo.setCountRow((int) lines);
+        //总页数
+        Integer pages= pageInfo.getCountRow() % pageInfo.getPageSize()==0?pageInfo.getCountRow() / pageInfo.getPageSize():pageInfo.getCountRow() / pageInfo.getPageSize()+1;
+        pageInfo.setCountPage(pages);
+        //查询example
+        List<Cup> cups=cupDao.selectByExample(cupExample);
+        return cups;
+    }
+}
